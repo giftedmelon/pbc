777a715516e9e457288725e1a4eb8fe8c1e11213
giftedmelon <lyx1231996@gmail.com>
2023-12-15 17:08:31 +0900

    1

--- script.js
+++ script.js
@@ -8,3 +8,17 @@
       });
     });
   });
+
+  document.addEventListener('DOMContentLoaded', function () {
+    var fabBtn = document.getElementById('fabBtn');
+    var fabContent = document.getElementById('fabContent');
+  
+    fabBtn.addEventListener('click', function () {
+      // Toggle the visibility of the fab content
+      if (fabContent.style.display === 'block') {
+        fabContent.style.display = 'none';
+      } else {
+        fabContent.style.display = 'block';
+      }
+    });
+  });
