847d0950f55fdb7d1b0911fd47b46fbbc11d2d67
giftedmelon <lyx1231996@gmail.com>
2023-12-16 12:00:02 +0900

    1

--- script.js
+++ script.js
@@ -87,78 +87,78 @@
   }
 
   document.addEventListener('selectionchange', function () {
-    var selectedText = getSelectedText();
-    if (selectedText) {
-      showFloatingWindow(selectedText);
-    } else {
-      hideFloatingWindow();
+      var selectedText = getSelectedText();
+      if (selectedText) {
+        showFloatingWindow(selectedText);
+      } else {
+        hideFloatingWindow();
+      }
+    });
+
+    function getSelectedText() {
+      if (window.getSelection) {
+        return window.getSelection().toString();
+      } else if (document.selection && document.selection.type !== 'Control') {
+        return document.selection.createRange().text;
+      }
+      return '';
     }
-  });
 
-  function getSelectedText() {
-    if (window.getSelection) {
-      return window.getSelection().toString();
-    } else if (document.selection && document.selection.type !== 'Control') {
-      return document.selection.createRange().text;
+    function showFloatingWindow(selectedText) {
+      var floatingWindow = document.getElementById('floating-window');
+      var selection = window.getSelection();
+
+      if (selection.rangeCount > 0) {
+        var range = selection.getRangeAt(0);
+        var rect = range.getBoundingClientRect();
+
+        floatingWindow.style.top = rect.bottom + window.scrollY + 'px';
+        floatingWindow.style.left = (rect.left + rect.right) / 2 + window.scrollX - floatingWindow.offsetWidth / 2 + 'px';
+
+        floatingWindow.style.display = 'block';
+
+        document.getElementById('comment-button').addEventListener('click', function () {
+          showCommentModal();
+        });
+
+        document.getElementById('highlight-button').addEventListener('click', function () {
+          highlightSelectedText(range);
+        });
+      }
     }
-    return '';
-  }
 
-  function showFloatingWindow(selectedText) {
-    var floatingWindow = document.getElementById('floating-window');
-    var selection = window.getSelection();
-
-    if (selection.rangeCount > 0) {
-      var range = selection.getRangeAt(0);
-      var rect = range.getBoundingClientRect();
-
-      floatingWindow.style.top = rect.bottom + window.scrollY + 'px';
-      floatingWindow.style.left = (rect.left + rect.right) / 2 + window.scrollX - floatingWindow.offsetWidth / 2 + 'px';
-
-      floatingWindow.style.display = 'block';
-
-      document.getElementById('comment-button').addEventListener('click', function () {
-        showCommentModal();
-      });
-
-      document.getElementById('highlight-button').addEventListener('click', function () {
-        highlightSelectedText(range);
-      });
+    function hideFloatingWindow() {
+      var floatingWindow = document.getElementById('floating-window');
+      floatingWindow.style.display = 'none';
     }
-  }
 
-  function hideFloatingWindow() {
-    var floatingWindow = document.getElementById('floating-window');
-    floatingWindow.style.display = 'none';
-  }
+    function showCommentModal() {
+      var commentModal = document.getElementById('comment-modal');
+      commentModal.classList.add('open'); // Add the 'open' class to show the modal
+    }
 
-  function showCommentModal() {
-    var commentModal = document.getElementById('comment-modal');
-    commentModal.classList.add('open'); // Add the 'open' class to show the modal
-  }
+    function closeCommentModal() {
+      var commentModal = document.getElementById('comment-modal');
+      commentModal.classList.remove('open'); // Remove the 'open' class to hide the modal
+    }
 
-  function closeCommentModal() {
-    var commentModal = document.getElementById('comment-modal');
-    commentModal.classList.remove('open'); // Remove the 'open' class to hide the modal
-  }
+    function submitComment() {
+      var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+      var selectedOptions = Array.from(checkboxes).filter(checkbox => checkbox.checked).map(checkbox => checkbox.name);
+      var commentText = document.getElementById('comment').value;
 
-  function submitComment() {
-    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    var selectedOptions = Array.from(checkboxes).filter(checkbox => checkbox.checked).map(checkbox => checkbox.name);
-    var commentText = document.getElementById('comment').value;
+      // You can perform further actions with the selected options and comment here
+      console.log('Selected Options:', selectedOptions);
+      console.log('Comment:', commentText);
 
-    // You can perform further actions with the selected options and comment here
-    console.log('Selected Options:', selectedOptions);
-    console.log('Comment:', commentText);
+      // Close the modal
+      closeCommentModal();
+    }
 
-    // Close the modal
-    closeCommentModal();
-  }
-
-  function highlightSelectedText(range) {
-    var span = document.createElement('span');
-    span.style.backgroundColor = 'red';
-    span.textContent = range.toString();
-    range.deleteContents();
-    range.insertNode(span);
-  }
+    function highlightSelectedText(range) {
+      var span = document.createElement('span');
+      span.style.backgroundColor = 'red';
+      span.textContent = range.toString();
+      range.deleteContents();
+      range.insertNode(span);
+    }
